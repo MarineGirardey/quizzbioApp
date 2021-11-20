d6807d62d351ff31857ea5ee706dce55989cca62
Marine Girardey <marinegirardey@Marines-MacBook-Air.local>
2021-11-20 22:07:52 +0100

    test

--- m2_web_project/settings.py
+++ m2_web_project/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'quizzbioApp/static'), ]
 MEDIA_ROOT = [os.path.join(BASE_DIR, "media"), ]
 MEDIA_URL = "/media/"
